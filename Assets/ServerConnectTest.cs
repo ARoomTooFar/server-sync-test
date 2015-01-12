4a0fa74c8d797b50c7c2a3bafcc602109cec12f8
Matthew Isaac Lean <isaaclean@creatibyte.com>
2015-01-12 02:13:11 -0800

    Game can now download from server

---
+++ Assets/ServerConnectTest.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ServerConnectTest : MonoBehaviour {
+
+	// Use this for initialization
+	IEnumerator Start () {
+		Debug.Log("yes");
+		WWW www = new WWW("http://localhost:8080/api/levels/-EsLHlQv43iWKsGDPd-nRQ==");
+		yield return www;
+		Debug.Log(www.text);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Upload.cs
@@ -0,0 +1,17 @@
+﻿ /*using UnityEngine;
+ using UnityEditor;
+ using System.Collections;
+  
+ [InitializeOnLoad]
+ public class Autorun {
+    static Autorun() {
+        Debug.Log("Upload Test");
+	    dlFile("http://localhost:8080/api/levels/-EsLHlQv43iWKsGDPd-nRQ==");
+     }
+
+    static IEnumerator dlFile (string url) {
+		WWW www = new WWW(url);
+		yield return www;
+	 	Debug.Log(www.text);
+	}
+ }*/
