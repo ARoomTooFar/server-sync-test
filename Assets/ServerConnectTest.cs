3bfb0b9c43bf7d2639bafe0a160e50d1381da155
Matthew Isaac Lean <isaaclean@creatibyte.com>
2015-01-12 05:17:35 -0800

    Progress on file upload

--- Assets/ServerConnectTest.cs
+++ Assets/ServerConnectTest.cs
@@ -2,13 +2,31 @@
 using System.Collections;
 
 public class ServerConnectTest : MonoBehaviour {
-
-	// Use this for initialization
+	
 	IEnumerator Start () {
-		Debug.Log("yes");
-		WWW www = new WWW("http://localhost:8080/api/levels/-EsLHlQv43iWKsGDPd-nRQ==");
-		yield return www;
-		Debug.Log(www.text);
+		Debug.Log("Hello world!");
+
+		//Test file download
+		WWW dlFile = new WWW ("http://localhost:8080/api/levels/-EsLHlQv43iWKsGDPd-nRQ==");
+		yield return dlFile;
+		Debug.Log (dlFile.text);
+
+		//Test file upload
+		string ulFilePath = Application.dataPath + "/upload-dis.txt";
+		Debug.Log (ulFilePath);
+		WWW ulFile = new WWW("file:///" + ulFilePath);
+		yield return ulFile;
+		Debug.Log (ulFile.GetType());
+
+		WWWForm postForm = new WWWForm ();
+		postForm.AddBinaryData ("file", ulFile.bytes, "upload-dis.txt", "text/plain");
+		Debug.Log (postForm.data);
+		WWW upload = new WWW ("http://localhost:8080/api/levels/", postForm);
+		yield return upload;
+		if (upload.error == null)
+			Debug.Log(upload.text);
+		else
+			Debug.Log(upload.error);
 	}
 	
 	// Update is called once per frame
