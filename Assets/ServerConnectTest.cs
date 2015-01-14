d6e9a7999dc3861f2d04761f8d655584c883af21
Matthew Isaac Lean <isaaclean@creatibyte.com>
2015-01-13 22:54:52 -0800

    Can send file data to server via post request

--- Assets/ServerConnectTest.cs
+++ Assets/ServerConnectTest.cs
@@ -1,26 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class ServerConnectTest : MonoBehaviour {
-	
+	public WWW ulFile;
 	IEnumerator Start () {
 		Debug.Log("Hello world!");
 
 		//Test file download
-		WWW dlFile = new WWW ("http://localhost:8080/api/levels/-EsLHlQv43iWKsGDPd-nRQ==");
+		/*WWW dlFile = new WWW ("http://localhost:8080/api/levels/-EsLHlQv43iWKsGDPd-nRQ==");
 		yield return dlFile;
-		Debug.Log (dlFile.text);
+		Debug.Log (dlFile.text);*/
 
 		//Test file upload
 		string ulFilePath = Application.dataPath + "/upload-dis.txt";
 		Debug.Log (ulFilePath);
-		WWW ulFile = new WWW("file:///" + ulFilePath);
+		ulFile = new WWW("file:///" + ulFilePath);
 		yield return ulFile;
-		Debug.Log (ulFile.GetType());
+		Debug.Log (ulFile.data);
 
 		WWWForm postForm = new WWWForm ();
-		postForm.AddBinaryData ("file", ulFile.bytes, "upload-dis.txt", "text/plain");
-		Debug.Log (postForm.data);
+		postForm.AddField ("filestr", "test123");
+		//postForm.AddBinaryData ("file", ulFile.bytes, "upload-dis.txt", "text/plain");
+		//Debug.Log (ulFile.bytes);
+
+		//Debug.Log (postForm.headers.Keys);
+		Debug.Log (postForm.headers["Content-Type"]);
+		foreach (String header in postForm.headers.Keys) {
+			Debug.Log (header);
+		}
+
 		WWW upload = new WWW ("http://localhost:8080/api/levels/", postForm);
 		yield return upload;
 		if (upload.error == null)
