7da6370a09ebaaedc531801bbe5b4e6b779de045
Matthew Isaac Lean <isaaclean@creatibyte.com>
2015-01-14 05:29:02 -0800

    Setup clean examples for level upload/download

--- Assets/ServerConnectTest.cs
+++ Assets/ServerConnectTest.cs
@@ -1,45 +1,39 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using System;
 
 public class ServerConnectTest : MonoBehaviour {
 	public WWW ulFile;
 	IEnumerator Start () {
-		Debug.Log("Hello world!");
-
-		//Test file download
-		/*WWW dlFile = new WWW ("http://localhost:8080/api/levels/-EsLHlQv43iWKsGDPd-nRQ==");
-		yield return dlFile;
-		Debug.Log (dlFile.text);*/
-
-		//Test file upload
-		string ulFilePath = Application.dataPath + "/upload-dis.txt";
-		Debug.Log (ulFilePath);
-		ulFile = new WWW("file:///" + ulFilePath);
-		yield return ulFile;
-		Debug.Log (ulFile.data);
-
+		/*
+		//Setup level upload example 1
 		WWWForm postForm = new WWWForm ();
-		postForm.AddField ("filestr", "test123");
-		//postForm.AddBinaryData ("file", ulFile.bytes, "upload-dis.txt", "text/plain");
-		//Debug.Log (ulFile.bytes);
+		postForm.AddField ("user_id", 123);
+		postForm.AddField ("level_name", "A Room To Fart In");
+		postForm.AddField ("live_level_data", "leveldatablahblahblah123");
+		*/
 
-		//Debug.Log (postForm.headers.Keys);
-		Debug.Log (postForm.headers["Content-Type"]);
-		foreach (String header in postForm.headers.Keys) {
-			Debug.Log (header);
-		}
+		/*
+		//Setup level upload example 2
+		WWWForm postForm = new WWWForm ();
+		postForm.AddField ("user_id", 456);
+		postForm.AddField ("level_name", "A Room Too Butts");
+		postForm.AddField ("live_level_data", "heyayayayayayaya");
+		*/
 
+		/*
+		//Do the API POST request at /api/levels to upload to the server
 		WWW upload = new WWW ("http://localhost:8080/api/levels/", postForm);
 		yield return upload;
 		if (upload.error == null)
 			Debug.Log(upload.text);
 		else
 			Debug.Log(upload.error);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+		*/
+
+		//Do the API GET request at /api/levels/levelId to download the level from the server
+		WWW www = new WWW("http://localhost:8080/api/levels/5785905063264256");
+		yield return www;
+		Debug.Log(www.text);
 	}
 }
