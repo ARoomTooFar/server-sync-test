f9c14fad487cd4cb3ff007a3f35a018f6b644e35
Matthew Isaac Lean <isaaclean@creatibyte.com>
2015-01-20 04:34:03 -0800

    Now communicates with server deployed in cloud

--- Assets/ServerConnectTest.cs
+++ Assets/ServerConnectTest.cs
@@ -9,18 +9,26 @@ public class ServerConnectTest : MonoBehaviour {
 
 	IEnumerator Start () {
 		//Do the API GET request at /api/levels/levelId to download the level from the server
-		WWW download = new WWW("http://localhost:8080/api/levels/5785905063264256");
+		WWW download = new WWW("https://artf-gs.appspot.com/api/levels/5629499534213120");
 		yield return download;
 		dlOutputField.ChangeText(download.text);
 		Debug.Log(download.text);
-		
+
 		/*
+		//Generate error for trying to download a level that doesn't exist
+		WWW download = new WWW("https://artf-gs.appspot.com/api/levels/1337");
+		yield return download;
+		dlOutputField.ChangeText(download.text);
+		Debug.Log(download.text);
+		*/
+
+		
 		//Setup level upload example 1
 		WWWForm postForm = new WWWForm ();
 		postForm.AddField ("user_id", 123);
 		postForm.AddField ("level_name", "A Room To Fart In");
 		postForm.AddField ("live_level_data", "leveldatablahblahblah123");
-		*/
+		
 
 		/*
 		//Setup level upload example 2
@@ -30,17 +38,24 @@ public class ServerConnectTest : MonoBehaviour {
 		postForm.AddField ("live_level_data", "heyayayayayayaya!!");
 		*/
 
+		/*
 		//Generate error for not defining "level_name" in upload
 		WWWForm postForm = new WWWForm ();
 		postForm.AddField ("user_id", 789);
 		postForm.AddField ("live_level_data", "this_will_be_denied");
+		*/
 
+		
 		//Do the API POST request at /api/levels to upload to the server
-		WWW uploadedData = new WWW ("http://localhost:8080/api/levels/", postForm);
+		WWW uploadedData = new WWW ("https://artf-gs.appspot.com/api/levels/", postForm);
+		
 
+		/*
 		//Generate error for sending a request to the incorrect URL
-		//WWW uploadedData = new WWW ("http://localhost:8080/api", postForm);
+		WWW uploadedData = new WWW ("https://artf-gs.appspot.com/api", postForm);
+		*/
 
+		
 		yield return uploadedData;
 		if (uploadedData.error == null) {
 			Debug.Log(uploadedData.text);
@@ -49,5 +64,6 @@ public class ServerConnectTest : MonoBehaviour {
 			Debug.Log(uploadedData.error);
 			ulOutputField.ChangeText(uploadedData.error);
 		}
+		
 	}
 }
