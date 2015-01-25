9a9b04ce528c8d4c67c4caf015ed0a70e12508bb
unknown <mlean@BRAID.adgpm.soe.ucsc.edu>
2015-01-25 07:28:07 -0800

    Small changes

--- Assets/ServerConnectTest.cs
+++ Assets/ServerConnectTest.cs
@@ -3,16 +3,15 @@ using System.Collections;
 using System;
 
 public class ServerConnectTest : MonoBehaviour {
-	public WWW ulFile;
 	public DLOutputText dlOutputField;
 	public ULOutputText ulOutputField;
 
 	IEnumerator Start () {
 		//Do the API GET request at /api/levels/levelId to download the level from the server
-		WWW download = new WWW("https://artf-gs.appspot.com/api/levels/5629499534213120");
-		yield return download;
-		dlOutputField.ChangeText(download.text);
-		Debug.Log(download.text);
+		WWW downloadedData = new WWW("https://artf-gs.appspot.com/api/levels/5629499534213120");
+		yield return downloadedData;
+		dlOutputField.ChangeText(downloadedData.text);
+		Debug.Log(downloadedData.text);
 
 		/*
 		//Generate error for trying to download a level that doesn't exist
@@ -22,13 +21,13 @@ public class ServerConnectTest : MonoBehaviour {
 		Debug.Log(download.text);
 		*/
 
-		
+		/*
 		//Setup level upload example 1
 		WWWForm postForm = new WWWForm ();
 		postForm.AddField ("user_id", 123);
 		postForm.AddField ("level_name", "A Room To Fart In");
 		postForm.AddField ("live_level_data", "leveldatablahblahblah123");
-		
+		*/
 
 		/*
 		//Setup level upload example 2
@@ -45,17 +44,17 @@ public class ServerConnectTest : MonoBehaviour {
 		postForm.AddField ("live_level_data", "this_will_be_denied");
 		*/
 
-		
+		/*
 		//Do the API POST request at /api/levels to upload to the server
 		WWW uploadedData = new WWW ("https://artf-gs.appspot.com/api/levels/", postForm);
-		
+		*/
 
 		/*
 		//Generate error for sending a request to the incorrect URL
 		WWW uploadedData = new WWW ("https://artf-gs.appspot.com/api", postForm);
 		*/
 
-		
+		/*
 		yield return uploadedData;
 		if (uploadedData.error == null) {
 			Debug.Log(uploadedData.text);
@@ -64,6 +63,6 @@ public class ServerConnectTest : MonoBehaviour {
 			Debug.Log(uploadedData.error);
 			ulOutputField.ChangeText(uploadedData.error);
 		}
-		
+		*/
 	}
 }
