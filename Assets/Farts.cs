be5998c2f9ee315f91c8e4da4a2d46e815e0cca4
Matthew Isaac Lean <isaaclean@creatibyte.com>
2015-03-03 02:08:06 -0800

    Timeout check is actually accurate for all functions that use it

--- Assets/Farts.cs
+++ Assets/Farts.cs
@@ -1,18 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Farts : MonoBehaviour {
-	const string SERVERURI = "http://localhost:8081"; //local server
-	//const string SERVERURI = "https://api-dot-artf-server.appspot.com"; //live server
-	const string LVLPATH = "/levels/";
-	const string GAMEACCTPATH = "/gameaccount/";
-    const float cancelTime = 50000f;
+public class Farts : MonoBehaviour
+{
+    const string SERVERURI = "http://localhost:8081"; //local server
+    //const string SERVERURI = "https://api-dot-artf-server.appspot.com"; //live server
+    const string LVLPATH = "/levels/";
+    const string GAMEACCTPATH = "/gameaccount/";
+    const float timeoutTime = 60f; //HTTP requests timeout after 1 minute
 
-	// Checks if returned data is valid or not. Returns true if the data is valid, false otherwise.
-	public bool dataCheck(string input) {
-		if (input == "") return false;
-		return true;
-	}
+    // Checks if returned data is valid or not. Returns true if the data is valid, false otherwise.
+    public bool dataCheck(string input)
+    {
+        if (input == "") return false;
+        return true;
+    }
 
     public WWW getLvlWww(string levelId)
     {
@@ -34,127 +36,133 @@ public class Farts : MonoBehaviour {
 
         return www;
     }
-	
-	public string getLvl(string levelId) {
-		WWW www = new WWW(SERVERURI + LVLPATH + levelId);
-        float elapsedTime = 0.0f;
-		
-		StartCoroutine(httpRequest(www));
-		while(www.isDone == false) {
-            if (elapsedTime >= cancelTime) {
-                Debug.LogError("ERROR: Request timeout");
-                return "";
-            }
 
-            elapsedTime += Time.deltaTime;
-            //Debug.Log("HTTP request time elapsed: " + elapsedTime);
-		}
-		
-		return www.text;
-	}
-	
-	public string newLvl(string gameAcctId, string machId, string liveLvlData="", string draftLvlData="") {
-        float elapsedTime = 0.0f;
-        
-        WWWForm form = new WWWForm();
-		form.AddField("game_acct_id", gameAcctId);
-		form.AddField("mach_id", machId);
-		if(liveLvlData != "")
-			form.AddField ("live_level_data", liveLvlData);
-		if(draftLvlData != "")
-			form.AddField ("draft_level_data", draftLvlData);
+    public string getLvl(string levelId)
+    {
+        WWW www = new WWW(SERVERURI + LVLPATH + levelId);
+        StartCoroutine(httpRequest(www));
 
-		WWW www = new WWW(SERVERURI + LVLPATH, form);
-		StartCoroutine(httpRequest(www));
+        float timeStart = Time.realtimeSinceStartup;
 
-        while (www.isDone == false)
+        while (!www.isDone)
         {
-            if (elapsedTime >= cancelTime)
+            if (Time.realtimeSinceStartup >= timeStart + timeoutTime)
             {
                 Debug.LogError("ERROR: Request timeout");
                 return "";
             }
-
-            elapsedTime += Time.deltaTime;
-            //Debug.Log("HTTP request time elapsed: " + elapsedTime);
+            //Debug.Log("HTTP request time elapsed: " + (Time.realtimeSinceStartup - timeStart));
         }
-		
-		return www.text;
-	}
 
-	public WWW updateLvl(string lvlId, string gameAcctId="", string liveLvlData="", string draftLvlData="") {
-		WWWForm form = new WWWForm();
-		form.AddField ("flag", "update");
-		if(gameAcctId != "")
-			form.AddField ("game_acct_id", gameAcctId);
-		if(liveLvlData != "")
-			form.AddField ("live_level_data", liveLvlData);
-		if(draftLvlData != "")
-			form.AddField ("draft_level_data", draftLvlData);
-		
-		WWW www = new WWW(SERVERURI + LVLPATH + lvlId, form);
-		StartCoroutine(httpRequest(www));
+        return www.text;
+    }
 
-        return www;
-	}
-	
-	public string delLvl(string lvlId) {
-        float elapsedTime = 0.0f;
+    public string newLvl(string gameAcctId, string machId, string liveLvlData = "", string draftLvlData = "")
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("game_acct_id", gameAcctId);
+        form.AddField("mach_id", machId);
+        if (liveLvlData != "")
+            form.AddField("live_level_data", liveLvlData);
+        if (draftLvlData != "")
+            form.AddField("draft_level_data", draftLvlData);
 
-		WWWForm form = new WWWForm();
-		form.AddField ("flag", "delete");
-		form.AddField ("level_id", lvlId);
+        WWW www = new WWW(SERVERURI + LVLPATH, form);
+        StartCoroutine(httpRequest(www));
+
+        float timeStart = Time.realtimeSinceStartup;
+
+        while (!www.isDone)
+        {
+            if (Time.realtimeSinceStartup >= timeStart + timeoutTime)
+            {
+                Debug.LogError("ERROR: Request timeout");
+                return "";
+            }
+            //Debug.Log("HTTP request time elapsed: " + (Time.realtimeSinceStartup - timeStart));
+        }
+
+        return www.text;
+    }
+
+    public WWW updateLvl(string lvlId, string gameAcctId = "", string liveLvlData = "", string draftLvlData = "")
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("flag", "update");
+        if (gameAcctId != "")
+            form.AddField("game_acct_id", gameAcctId);
+        if (liveLvlData != "")
+            form.AddField("live_level_data", liveLvlData);
+        if (draftLvlData != "")
+            form.AddField("draft_level_data", draftLvlData);
 
         WWW www = new WWW(SERVERURI + LVLPATH + lvlId, form);
-		StartCoroutine(httpRequest(www));
+        StartCoroutine(httpRequest(www));
 
-        while (www.isDone == false)
+        return www;
+    }
+
+    public string delLvl(string lvlId)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("flag", "delete");
+        form.AddField("level_id", lvlId);
+
+        WWW www = new WWW(SERVERURI + LVLPATH + lvlId, form);
+        StartCoroutine(httpRequest(www));
+
+        float timeStart = Time.realtimeSinceStartup;
+
+        while (!www.isDone)
         {
-            if (elapsedTime >= cancelTime)
+            if (Time.realtimeSinceStartup >= timeStart + timeoutTime)
             {
                 Debug.LogError("ERROR: Request timeout");
                 return "";
             }
-
-            elapsedTime += Time.deltaTime;
-            //Debug.Log("HTTP request time elapsed: " + elapsedTime);
+            //Debug.Log("HTTP request time elapsed: " + (Time.realtimeSinceStartup - timeStart));
         }
-		
-		return www.text;
-	}
 
-	public string login(string gameAcctName, string gameAcctPass) {
-		float elapsedTime = 0.0f;
-		
-		WWWForm form = new WWWForm();
-		form.AddField ("game_acct_name", gameAcctName);
-		form.AddField ("game_acct_password", gameAcctPass);
-		
-		WWW www = new WWW(SERVERURI + GAMEACCTPATH + "login", form);
-		StartCoroutine(httpRequest(www));
-		
-		while (www.isDone == false)
-		{
-			if (elapsedTime >= cancelTime)
-			{
-				Debug.LogError("ERROR: Request timeout");
-				return "";
-			}
-			
-			elapsedTime += Time.deltaTime;
-			//Debug.Log("HTTP request time elapsed: " + elapsedTime);
-		}
-		
-		return www.text;
-	}
-	
-	IEnumerator httpRequest(WWW www) {
-		yield return www;
-		
-		if (www.error == null) {
-			Debug.Log("COROUTINE WWW SUCCESS: " + www.text);
-		} else {
+        return www.text;
+    }
+
+    public string login(string gameAcctName, string gameAcctPass)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("game_acct_name", gameAcctName);
+        form.AddField("game_acct_password", gameAcctPass);
+
+        WWW www = new WWW(SERVERURI + GAMEACCTPATH + "login", form);
+        StartCoroutine(httpRequest(www));
+
+        float timeStart = Time.realtimeSinceStartup;
+
+        while (!www.isDone)
+        {
+            if (Time.realtimeSinceStartup >= timeStart + timeoutTime)
+            {
+                Debug.LogError("ERROR: Request timeout");
+                return "";
+            }
+            //Debug.Log("HTTP request time elapsed: " + (Time.realtimeSinceStartup - timeStart));
+        }
+
+        return www.text;
+    }
+
+    IEnumerator httpRequest(WWW www)
+    {
+        //Debug.Log("COROUTINE WWW REQUEST START");
+
+        yield return www;
+
+        /*if (www.error == null)
+        {
+            Debug.Log("COROUTINE WWW SUCCESS: " + www.text);
+        }
+        else
+        {
             Debug.Log("COROUTINE WWW ERROR: " + www.error);
-		}
-	}
+        }*/
+    }
 }
